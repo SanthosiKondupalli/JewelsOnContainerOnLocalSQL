38f5a80796c4db614aa8ee4752dd55c95bfec356
SanthosiKondupalli <santhosi.kondupalli@gmail.com>
2021-07-21 17:19:40 -0700

    Add project files.

---
+++ ProductCatalogsAPI/Domain/CatalogBrand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogsAPI.Domain
+{
+    public class CatalogBrand
+    {
+        public int Id { get; set; }
+        public String Brand { get; set; }
+    }
+}
